685b513aba6dbc316aa006cc1b9871c5aefc7462
NdongesitBassey <ndongesit.bassey@slu.edu>
2025-04-16 21:15:30 -0500

    Update streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-# App title
+# Title
 st.title("Automobile Dataset Analysis")
 
-# Load dataset
+# Load data
 path = 'https://raw.githubusercontent.com/klamsal/Fall2024Exam/refs/heads/main/CleanedAutomobile.csv'
 df = pd.read_csv(path)
 
-# Display dataset preview
+# Show dataframe
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
 
@@ -22,48 +21,38 @@ st.text(df.dtypes)
 st.subheader("Correlation Matrix")
 st.dataframe(df.corr(numeric_only=True))
 
-# Visualizations
-st.subheader("Regression Plots")
+# Custom scatter plots
+def plot_scatter(x, y):
+    fig, ax = plt.subplots()
+    ax.scatter(df[x], df[y], alpha=0.6)
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
+    ax.set_title(f'{x} vs {y}')
+    st.pyplot(fig)
+    st.write(df[[x, y]].corr())
 
-# Engine-size vs Price
+st.subheader("Visualizations")
+
 st.write("**Engine Size vs Price**")
-fig1, ax1 = plt.subplots()
-sns.regplot(x="engine-size", y="price", data=df, ax=ax1)
-st.pyplot(fig1)
-st.write(df[["engine-size", "price"]].corr())
+plot_scatter("engine-size", "price")
 
-# Highway-mpg vs Price
 st.write("**Highway MPG vs Price**")
-fig2, ax2 = plt.subplots()
-sns.regplot(x="highway-mpg", y="price", data=df, ax=ax2)
-st.pyplot(fig2)
-st.write(df[["highway-mpg", "price"]].corr())
+plot_scatter("highway-mpg", "price")
 
-# Peak RPM vs Price
 st.write("**Peak RPM vs Price**")
-fig3, ax3 = plt.subplots()
-sns.regplot(x="peak-rpm", y="price", data=df, ax=ax3)
-st.pyplot(fig3)
-st.write(df[["peak-rpm", "price"]].corr())
+plot_scatter("peak-rpm", "price")
 
-# Boxplots
+# Boxplot with matplotlib
+def plot_box(x, y):
+    fig, ax = plt.subplots()
+    df.boxplot(column=y, by=x, ax=ax)
+    plt.title(f'{y} by {x}')
+    plt.suptitle("")
+    st.pyplot(fig)
+
 st.subheader("Boxplots")
 
-# Body Style vs Price
-st.write("**Body Style vs Price**")
-fig4, ax4 = plt.subplots()
-sns.boxplot(x="body-style", y="price", data=df, ax=ax4)
-st.pyplot(fig4)
-
-# Engine Location vs Price
-st.write("**Engine Location vs Price**")
-fig5, ax5 = plt.subplots()
-sns.boxplot(x="engine-location", y="price", data=df, ax=ax5)
-st.pyplot(fig5)
-
-# Drive Wheels vs Price (if included)
-if 'drive-wheels' in df.columns:
-    st.write("**Drive Wheels vs Price**")
-    fig6, ax6 = plt.subplots()
-    sns.boxplot(x="drive-wheels", y="price", data=df, ax=ax6)
-    st.pyplot(fig6)
+plot_box("body-style", "price")
+plot_box("engine-location", "price")
+if "drive-wheels" in df.columns:
+    plot_box("drive-wheels", "price")
